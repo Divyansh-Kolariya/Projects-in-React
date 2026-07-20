2889eaad897ee1d6f3db68a2a30d58e253e49234
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-20 17:45:11 +0530

    Add main App component with state management for todo items

---
+++ 01-TodoApp/src/App.jsx
@@ -0,0 +1,36 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item added: ${itemName} Date: ${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleted = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+  return (
+    <>
+      <center classNameName="todo-container">
+        <AppName />
+        <AddTodo onNewItem={handleNewItem} />
+        {todoItems.length === 0 && <WelcomeMessage />}
+        <TodoItems todoItems={todoItems} onDeleteClick={handleDeleted} />
+      </center>
+    </>
+  );
+}
+
+export default App;
