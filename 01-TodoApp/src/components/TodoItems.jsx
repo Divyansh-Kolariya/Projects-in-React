2633de571c22ceeba3552990588379aec59c38f6
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-20 17:46:15 +0530

    Add TodoItems component for rendering a list of todo items

---
+++ 01-TodoApp/src/components/TodoItems.jsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+
+function TodoItems({ todoItems, onDeleteClick }) {
+  return (
+    <>
+      <div className="items-container">
+        {todoItems.map((item) => (
+          <TodoItem
+            todoName={item.name}
+            todoDate={item.dueDate}
+            onDeleteClick={onDeleteClick}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default TodoItems;
