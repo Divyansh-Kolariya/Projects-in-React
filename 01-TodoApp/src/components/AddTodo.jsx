0b61446cd0e348ef32035dad3ed9781331d3ac2d
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-20 17:45:37 +0530

    Add AddTodo component for managing new todo items with name and date inputs

---
+++ 01-TodoApp/src/components/AddTodo.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [todoDate, setTodoDate] = useState();
+
+  const handleNameChange = (e) => {
+    setTodoName(e.target.value);
+  };
+  const handleDateChange = (e) => {
+    setTodoDate(e.target.value);
+  };
+
+  const handleAddBtnCliked = () => {
+    if (todoName.trim() !== undefined && todoDate !== undefined) {
+      onNewItem(todoName, todoDate);
+      setTodoDate("");
+      setTodoName("");
+    }
+  };
+
+  return (
+    <>
+      <div class="row gogo-row">
+        <div class="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div class="col-4">
+          <input type="date" value={todoDate} onChange={handleDateChange} />
+        </div>
+        <div class="col-2">
+          <button
+            type="button"
+            class="btn btn-success gogo-btn"
+            onClick={handleAddBtnCliked}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AddTodo;
