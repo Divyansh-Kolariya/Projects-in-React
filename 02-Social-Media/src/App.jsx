557f3c6b9d6474d9b1962028a811e95959cbd639
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-29 20:20:11 +0530

    feat: implement main layout with Sidebar, Header, and Footer components; add Form and PostList components

--- 02-Social-Media/src/App.jsx
+++ 02-Social-Media/src/App.jsx
@@ -1,16 +1,24 @@
-
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <>
-     <Header />
-     <Footer />
-    </>
-  )
+    <div className="app-container">
+      <Sidebar selectedTabs = {selectedTab}/>
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <Form />}
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
