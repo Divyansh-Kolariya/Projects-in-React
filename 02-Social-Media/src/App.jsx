5469c56b52b2a8770b9cf334450fc9c791cbce52
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-29 19:15:26 +0530

    feat: add Header and Footer components with Bootstrap styling

--- 02-Social-Media/src/App.jsx
+++ 02-Social-Media/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <div>GOGO</div>
+     <Header />
+     <Footer />
     </>
   )
 }
